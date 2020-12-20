a86a0e63cf2800f74c5976d4bd25a31d5f8159d1
Eugene <zhenya-kiselev-97@mail.ru>
2020-12-20 20:41:10 +0300

    Initial Commit

---
+++ VKNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKNews
+//
+//  Created by Eugene Kiselev on 20.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
